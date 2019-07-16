6f70700a6b5e30874f3eeb771355ca1e87c7dac8
lishen <lishen_ls@outlook.com>
2019-07-16 23:35:24 +0800

    创建计算器按键布局

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+function Button({clickHandle,value,type}) {
+  return (
+    <div className={"button "+type}>
+      {value}
+    </div>
+  )
+}
+export default Button;
---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "../Button";
+import "./index.css";
+function Wrapper() {
+  return (
+    <div className="wrapper">
+      <Button type="func" value={"AC"} />
+      <Button type="func" value={"DEL"} />
+      <Button type="character" value={"%"} />
+      <Button type="character" value={"/"} />
+      <Button value={7} />
+      <Button value={8} />
+      <Button value={9} />
+      <Button type="character" value={"*"} />
+      <Button value={4} />
+      <Button value={5} />
+      <Button value={6} />
+      <Button type="character" value={"-"} />
+      <Button value={1} />
+      <Button value={2} />
+      <Button value={3} />
+      <Button type="character" value={"+"} />
+      <Button type="character" value={"+/-"} />
+      <Button value={0} />
+      <Button type="character" value={"."} />
+      <Button type="special" value={"="} />
+    </div>
+  );
+}
+export default Wrapper;
