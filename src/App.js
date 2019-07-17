c0baa90c24458cb77e15b968c3992f24328846e4
lishen <lishen_ls@outlook.com>
2019-07-17 17:07:45 +0800

    稍作修改

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Wrapper from "./components/Wrapper";
+import Result from "./components/Result";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Result></Result>
       <Wrapper></Wrapper>
     </div>
   );
